fe9d834299041671c9bb7abad55eed15fbda777e
Riddhi J. Pitliya <riddhi.pitliyajain@gmail.com>
2024-03-08 11:42:43 +0000

    restarting for more testing

--- agency_twoagents.py
+++ agency_twoagents.py
@@ -56,8 +56,7 @@ def create_A(p_outcome):
     
     # p(outcome | self_pos, other_pos, self_notpress, other_press)
     #                       present    absent
-    A_outcome[:,0,0,1,0] = [p_outcome, 1.0-p_outcome]
-    # A_outcome[:,0,0,1,0] = [1.0-p_outcome, p_outcome]
+    A_outcome[:,0,0,1,0] = [1.0-p_outcome, p_outcome]
     
     # p(outcome | self_pos, other_pos, self_press, other_notpress)
     #                       present    absent
